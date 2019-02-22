eaa607448b6f27326d150045267db3d3f11579f8
YangDR <ydrml@hotmail.com>
2019-02-22 18:33:17 +0800

    init

---
+++ World.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ydrml on 2019/2/22.
+//
+
+#include "World.h"
---
+++ World.h
@@ -0,0 +1,14 @@
+//
+// Created by ydrml on 2019/2/22.
+//
+
+#ifndef PM1_SDK_WORLD_H
+#define PM1_SDK_WORLD_H
+
+
+class World {
+
+};
+
+
+#endif //PM1_SDK_WORLD_H
