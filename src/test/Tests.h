4d05080512b41ca7801e08d1d23157ee607fb7ea
YangDR <ydrml@hotmail.com>
2019-02-26 10:27:30 +0800

    分离测试代码

---
+++ src/test/Tests.h
@@ -0,0 +1,22 @@
+//
+// Created by ydrml on 2019/2/26.
+//
+
+#ifndef PM1_SDK_TESTS_H
+#define PM1_SDK_TESTS_H
+
+
+namespace autolabor {
+	namespace pm1 {
+		namespace test {
+			void test_string_print();
+			
+			void test_serial_port();
+			
+			void test_crc_check();
+		}
+	}
+}
+
+
+#endif //PM1_SDK_TESTS_H
