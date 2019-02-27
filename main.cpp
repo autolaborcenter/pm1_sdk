7181a79eddd0a46d34603b98832e5dfc153517ea
YangDR <ydrml@hotmail.com>
2019-02-27 17:58:30 +0800

    整理 main

--- main.cpp
+++ main.cpp
@@ -2,18 +2,11 @@
 // Created by ydrml on 2019/2/22.
 //
 
-#include <iostream>
-#include "src/test/test.h"
-#include "src/main/pm1/time_extensions.h"
-#include "src/main/pm1/extensions.h"
 #include "src/main/pm1/api.h"
-#include "src/main/pm1/internal/can/can_message.h"
-#include "src/main/pm1/internal/can/can_define.h"
 
 using namespace autolabor::pm1;
-using namespace autolabor::pm1::test;
 
 int main() {
-	autolabor::pm1::initialize("com4");
-	autolabor::pm1::go_arc(1, 1, 3.14);
+	initialize("com4");
+	go_arc(1, 1, 1);
 }
