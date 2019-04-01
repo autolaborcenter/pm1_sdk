ef659438d0d45790b5edcf3991df426d83ca2b23
YdrMaster <ydrml@hotmail.com>
2019-04-01 11:58:32 +0800

    delete yield

--- src/main/internal/chassis.cc
+++ src/main/internal/chassis.cc
@@ -119,7 +119,7 @@ chassis::chassis(const std::string &port_name,
 				task_time[2] = _now;
 			}
 			
-			std::this_thread::yield();
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
 		}
 	}).detach();
 	
