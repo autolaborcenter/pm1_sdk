04d483f546cc4722f140cf1302b02842d0272d1c
YdrMaster <ydrml@hotmail.com>
2019-05-31 08:10:57 +0800

    update

---
+++ src/main/internal/chassis_state_t.cc
@@ -0,0 +1,5 @@
+//
+// Created by ydrml on 2019/5/31.
+//
+
+#include "chassis_state_t.hh"
---
+++ src/main/internal/chassis_state_t.hh
@@ -0,0 +1,16 @@
+//
+// Created by ydrml on 2019/5/31.
+//
+
+#ifndef PM1_SDK_CHASSIS_STATE_T_HH
+#define PM1_SDK_CHASSIS_STATE_T_HH
+
+
+
+class chassis_state_t {
+
+};
+
+
+
+#endif //PM1_SDK_CHASSIS_STATE_T_HH
